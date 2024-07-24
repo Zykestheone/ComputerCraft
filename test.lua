9e37ca7189dafc5ce53f95ad35c15c4cb13d4573
Zykestheone <alexsstewart@hotmail.com>
2024-07-24 10:32:51 +0100

    idk

--- test.lua
+++ test.lua
@@ -1,5 +1,16 @@
-local ws = http.websocket("ws://localhost:5656")
-while true do
-    local data = ws.receive()
-    assert(loadstring(data)) ()
+local ws,err = http.websocket("ws://localhost:5656")
+if ws then
+    while true do
+        local data = ws.receive()
+        if data ~= "getfuel" then
+            local cmd = assert(loadstring(data))
+            cmd()
+        end
+        elseif data == "getfuel" then
+            local flevel =turtle.getFuelLevel()
+            ws.send(flevel)
+        end
+    end
+elseif err then
+    print(err)
 end
