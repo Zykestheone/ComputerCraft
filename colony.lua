cab6ba886e3ccb5352a4bef5ecd936469e92434e
Zykestheone <alexsstewart@hotmail.com>
2024-07-25 20:04:58 +0100

    test

---
+++ colony.lua
@@ -0,0 +1,13 @@
+local colony = peripheral.wrap("back")
+while true do
+    term.clear()
+    print("Building Sites: ".. colony.amountOfConstructionSites())
+    print("Citizens: ".. colony.amountOfCitizens())
+    local underAttack = "No"
+    if colony.isUnderAttack() then
+        underAttack = "Yes"
+    end
+    print("Is under attack? ".. underAttack)
+    print("Overall happiness: ".. math.floor(colony.getHappiness()))
+    print("Amount of graves: ".. colony.amountOfGraves())
+end
