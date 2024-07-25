3ffed48268c5f29e84b557282b44890c578176c7
Zykestheone <alexsstewart@hotmail.com>
2024-07-25 20:44:29 +0100

    Colony

--- colony.lua
+++ colony.lua
@@ -9,7 +9,7 @@ while true do
     print(colony.getColonyName().." Stats")
     print("--------------------------")
     print("")
-    print("Building Sites: ".. colony.amountOfConstructionSites())
+    print("Construction Sites: ".. colony.amountOfConstructionSites())
     print("Citizens: ".. colony.amountOfCitizens().."/"..colony.maxOfCitizens())
     print("Overall happiness: ".. math.floor(colony.getHappiness()))
     print("Amount of graves: ".. colony.amountOfGraves())
