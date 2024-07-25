98a238a6d7c8c7bc2487d85e0efaf2a1c28f34dd
Zykestheone <alexsstewart@hotmail.com>
2024-07-25 20:42:59 +0100

    Colony

--- colony.lua
+++ colony.lua
@@ -10,12 +10,7 @@ while true do
     print("--------------------------")
     print("")
     print("Building Sites: ".. colony.amountOfConstructionSites())
-    print("Citizens: ".. colony.amountOfCitizens())
-    local underAttack = "No"
-    if colony.isUnderAttack() then
-        underAttack = "Yes"
-    end
-    print("Is under attack? ".. underAttack)
+    print("Citizens: ".. colony.amountOfCitizens().."/"..colony.maxOfCitizens())
     print("Overall happiness: ".. math.floor(colony.getHappiness()))
     print("Amount of graves: ".. colony.amountOfGraves())
     os.sleep(60)
