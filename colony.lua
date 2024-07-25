246622083bd38fa13f708cd9fb62bc97dcefff2d
Zykestheone <alexsstewart@hotmail.com>
2024-07-25 20:07:37 +0100

    colony

--- colony.lua
+++ colony.lua
@@ -10,4 +10,5 @@ while true do
     print("Is under attack? ".. underAttack)
     print("Overall happiness: ".. math.floor(colony.getHappiness()))
     print("Amount of graves: ".. colony.amountOfGraves())
+    os.sleep(60)
 end
