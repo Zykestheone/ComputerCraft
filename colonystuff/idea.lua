81312881cf3d79a9e42c73da4abf4d701d5f78dc
Zykestheone <alexsstewart@hotmail.com>
2024-07-27 16:32:44 +0100

    test

--- colonystuff/idea.lua
+++ colonystuff/idea.lua
@@ -19,9 +19,22 @@ function checkME(item)
     local itemDetails, err = mebridge.getItem({name = item.name})
     if itemDetails and itemDetails.amount and itemDetails.amount >= item.count then
         print("Item is Avaiable")
+        exportItem(item)
     else
         print("Item not Available")
     end
 end
 
+function exportItem()
+    local direction = "back"
+    local exportedCount, err = mebridge.exportItem({name = item.name, count = item.count}, direction)
+    if exportedCount and exportedCount > 0 then
+        print("Exported Item")
+    else
+        print("Failled to export item " .. (err or "Unknown Error"))
+    end
+end
+
+
+end
 request()
