b6e01cc7136ac6d127eaa9e4c2e42b4c37baf0c4
Zykestheone <alexsstewart@hotmail.com>
2024-07-28 09:41:26 +0100

    test

--- colonystuff/BuildRequest.lua
+++ colonystuff/BuildRequest.lua
@@ -1,6 +1,11 @@
 local me = peripheral.find("meBridge")
 local colony = peripheral.find("colonyIntegrator")
 
+function termclear()
+    term.clear()
+    term.setCursorPos(1,1)
+end
+
 function workOrders()
     local jobids = colony.getWorkOrders()
     
@@ -15,11 +20,38 @@ function workOrders()
 end
 
 function checkJobID(jobids)
-    print("Please Enter Job ID: ")
+    print("Please Enter Job ID:")
     local inputID = tonumber(read())
-    print(inputID)
+
+    for i, jobid in ipairs(jobids) do
+        if jobid.id == inputID then
+            print("Getting Resources")
+            termclear()
+            getResources(inputID)
+            return
+        end
+    end
+    print("Invalid ID")
 end
 
+function getResourcesForJob(jobID)
+    local resources = colony.getWorkOrderResources(jobID)
+    
+    if resources then
+        print("Resources needed for Job ID " .. jobID .. ":")
+        for i, resource in ipairs(resources) do
+            print("Item: " .. resource.item)
+            print("Display Name: " .. resource.displayName)
+            print("Status: " .. resource.status)
+            print("Needed: " .. resource.needed)
+            print("Available: " .. tostring(resource.available))
+            print("Delivering: " .. tostring(resource.delivering))
+            print("--------------------")
+        end
+    else
+        print("No resources found for Job ID " .. jobID .. " or the Job ID does not exist.")
+    end
+end
 
 local jobids = workOrders()
 checkJobID(jobids)
