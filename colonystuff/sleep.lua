2d6c4e7451d79d0868164443e02e4d9aba1449ba
Zykestheone <40908530+Zykestheone@users.noreply.github.com>
2025-05-05 00:12:47 +0100

    Update sleep.lua

--- colonystuff/sleep.lua
+++ colonystuff/sleep.lua
@@ -5,14 +5,20 @@ end
 
 local Citizens = Colony.getCitizens()
 for i,citizen in ipairs(Citizens) do
+    local awakeCount = 0
     local location = citizen.location
     local bedPos = citizen.bedPos
-
-    if location.x == bedPos.x then
-        if location.y == bedPos.y then
-            if location.z == bedPos.y then
-                print("Citizen in bed")
+    local work = citizen.work
+    local jobType = work and work.type or ""
+    if jobType ~= "guardtower" and jobType ~= "barrackstower" then
+        if location.x == bedPos.x then
+            if location.y == bedPos.y then
+                if location.z == bedPos.z then
+                    awakeCount = awakeCount + 1
+                end
             end
         end
     end
+
+    print(awakeCount)
 end
