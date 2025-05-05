b212ac2331b9e3b753c73528152f70f01a5840eb
Zykestheone <40908530+Zykestheone@users.noreply.github.com>
2025-05-05 22:35:01 +0100

    Update inventory2.lua

--- colonystuff/inventory2.lua
+++ colonystuff/inventory2.lua
@@ -1,18 +1,47 @@
 local manager = peripheral.find("inventoryManager")
 local items = manager.getItems()
-local validTags = {
+
+local wantedTags = {
     ["minecraft:item/c:gems"] = true,
     ["minecraft:item/c:raw_materials"] = true,
     ["minecraft:item/c:dusts"] = true,
     ["minecraft:item/minecraft:coals"] = true
 }
+local unwantedTags = {
+    ["minecraft:item/c:cobblestones"] = true,
+    ["minecraft:item/c:dirt"] = true,
+    ["minecraft:item/c:gravels"] = true
+}
+local unwantedItems = {
+    ["minecraft:granite"] = true,
+    ["minecraft:andesite"] = true,
+    ["minecraft:diorite"] = true,
+    ["minecraft:tuff"] = true,
+    ["minecraft:flint"] = true
+}
 
-for i,item in ipairs(items) do
+for _, item in ipairs(items) do
+    local isWanted = false
+    local isUnwanted = unwantedItems[item.name] or false
+
     for _, tag in ipairs(item.tags) do
-        if validTags[tag] then
-            local removed = manager.removeItemFromPlayer("right", {name = item.name, fromSlot = item.slot, count = item.count})
-            print("Removed " .. removed .. " of " .. item.name)
-            break
+        if unwantedTags[tag] then
+            isUnwanted = true
         end
+        if wantedTags[tag] then
+            isWanted = true
+        end
+    end
+
+    if isWanted and not isUnwanted then
+        local removed = manager.removeItemFromPlayer("right", {
+            name = item.name, fromSlot = item.slot, count = item.count
+        })
+        print("Removed wanted: " .. removed .. " of " .. item.name)
+    elseif isUnwanted then
+        local removed = manager.removeItemFromPlayer("up", {
+            name = item.name, fromSlot = item.slot, count = item.count
+        })
+        print("Removed unwanted: " .. removed .. " of " .. item.name)
     end
 end
